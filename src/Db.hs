58ee04b6ec360847d6082dbdb0b9d9b701222bd5
mathfuru <mathfuru@gmail.com>
2013-05-29 23:39:56 +0900

    Add a view to show log table. (#5)

--- src/Db.hs
+++ src/Db.hs
@@ -6,6 +6,7 @@ module Db where
 import Database.Persist
 import Database.Persist.TH
 import Database.Persist.Sqlite
+import Control.Monad.IO.Class (liftIO)
 
 import Type
 
@@ -24,7 +25,13 @@ RLog
 |]
 
 saveToDb :: RailsLog -> IO ()
-saveToDb rails_log = withSqliteConn "foo.db" $ runSqlConn $ do
+saveToDb rails_log = withSqliteConn "foo.db" $ (runSqlConn :: SqlPersist IO a -> Connection -> IO a) $ do
     runMigration migrateAll
     actionId <- insert $ Action (lookup "controller" rails_log) (lookup "action" rails_log) (lookup "method" rails_log) (lookup "code" rails_log) (lookup "time" rails_log)
     mapM_ insert $ map (((flip RLog) actionId) . snd) $ filter ((flip (==) "other") . fst) rails_log
+
+readFromDb :: IO [Entity RLog]
+readFromDb = withSqliteConn "foo.db" $ (runSqlConn :: SqlPersist IO a -> Connection -> IO a) $ selectList [] []
+
+readFromDb' :: IO [String]
+readFromDb' = readFromDb >>= (return . map (rLogContent . entityVal))
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall #-}
 
-import Manager
+import Web.Scotty
+import Data.Monoid
+import Control.Monad.IO.Class
+import qualified Data.Text.Lazy as T
 
-main :: IO ()
-main = parseLog "log/development.log"
+import Manager
+import Db
+
+main = scotty 3000 $ do
+     get "/" $ do
+         rs <- liftIO readFromDb'
+         html $ T.pack ("<table>" ++ (concat $ map (\e -> "<tr><td>" ++ e ++ "</td></tr>") rs) ++ "</table>")
+
+     post "/flush" $ do
+         liftIO $ parseLog "log/development.log"
+         html ""
